ee5cc5ab8f559f789c47e7a6f08b51b219e7fa5c
unknown <shaicokohn@gmail.com>
2023-11-24 13:55:48 -0300

    Unconfirmed options routes

--- src/routes/unconfirmedOptionsRoutes.js
+++ src/routes/unconfirmedOptionsRoutes.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const { getUnconfirmedOptions, postUnconfirmedOptions, removeUnconfirmedOptions } = require('../controllers/unconfirmedOptionsControllers')
+
 
 const router = express.Router()
+
+router.get('/all', getUnconfirmedOptions)
+router.post('/postUnconfirmed', postUnconfirmedOptions)
+router.delete('/deleteUnconfirmed', removeUnconfirmedOptions)
 
 module.exports = router 
